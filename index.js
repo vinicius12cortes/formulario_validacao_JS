3b927b6375a5d20e593bc7709ad20bb99a1481c6
ProfessorRafaelKruger <144643170+ProfessorRafaelKruger@users.noreply.github.com>
2023-09-26 17:13:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -14,59 +14,11 @@ function checkForm() {
     console.log("O formulário está validado e pronto para o envio!");
   } else {
     // executa a validação de todos os campos para dar o feedback ao usuário do que falta preencher
-    checkInputUsername(document.querySelector("#username"));
-    checkInputEmail(document.querySelector("#email"));
-    checkInputPassword(document.querySelector("#password"));
-    checkInputPasswordConfirmation(
-      document.querySelector("#password-confirmation")
-    );
   }
 }
 
-function checkInputUsername(username) {
-  const usernameValue = username.value;
-  if (usernameValue === "") {
-    setErrorFor(username, "O nome de usuário é obrigatório.");
-  } else {
-    setSucessFor(username);
-  }
-}
-function checkInputEmail(email) {
-  const emailValue = email.value;
-  if (emailValue === "") {
-    setErrorFor(email, "O email é obrigatório.");
-  } else if (!checkEmail(emailValue)) {
-    setErrorFor(email, "Por favor, insira um email válido.");
-  } else {
-    setSucessFor(email);
-  }
-}
-function checkInputPassword(password) {
-  const passwordValue = password.value;
-  if (passwordValue === "") {
-    setErrorFor(password, "A senha é obrigatória.");
-  } else if (passwordValue.length < 6) {
-    setErrorFor(password, "A senha precisa ter no mínimo 6 caracteres.");
-  } else {
-    setSucessFor(password);
-  }
-}
-function checkInputPasswordConfirmation(passwordConfirmation) {
-  const passwordConfirmationValue = passwordConfirmation.value;
-  const passwordValue = document.querySelector("#password").value;
-  if (passwordConfirmationValue === "") {
-    setErrorFor(passwordConfirmation, "A confirmação da senha é obrigatória.");
-  } else if (passwordConfirmationValue.length < 6) {
-    setErrorFor(
-      passwordConfirmation,
-      "A senha precisa ter no mínimo 6 caracteres."
-    );
-  } else if (!(passwordConfirmationValue === passwordValue)) {
-    setErrorFor(passwordConfirmation, "As senhas não são iguais.");
-  } else {
-    setSucessFor(passwordConfirmation);
-  }
-}
+
+
 
 function setErrorFor(input, message) {
   const formControl = input.parentElement;
